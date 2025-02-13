c0cc92848e389e97cee0c934fc709b170d45588e
phildaponte <p.daponte@outlook.com>
2025-02-12 23:03:16 -0500

    fix: revert to original DiceBox implementation

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,5 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
-const fs = require('fs');
-
-// Read the WASM file
-const wasmPath = path.resolve(__dirname, 'node_modules/@3d-dice/dice-box/dist/assets/ammo/ammo.wasm.wasm');
-const wasmBinary = fs.readFileSync(wasmPath);
-const wasmBase64 = wasmBinary.toString('base64');
 
 module.exports = {
   mode: 'production',
@@ -20,15 +14,7 @@ module.exports = {
     },
     globalObject: 'this',
     clean: true,
-  },
-  optimization: {
-    runtimeChunk: false,
-    splitChunks: {
-      cacheGroups: {
-        default: false,
-        vendors: false,
-      },
-    },
+    publicPath: 'auto',
   },
   module: {
     rules: [
@@ -55,6 +41,13 @@ module.exports = {
         generator: {
           filename: 'assets/themes/[path][name][ext]'
         }
+      },
+      {
+        test: /\.wasm$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/ammo/[name][ext]'
+        }
       }
     ]
   },
@@ -67,38 +60,13 @@ module.exports = {
           globOptions: {
             ignore: ['**/.DS_Store'],
           },
+        },
+        {
+          from: 'node_modules/@3d-dice/dice-box/dist/assets/ammo',
+          to: 'assets/ammo'
         }
       ]
-    }),
-    {
-      apply: (compiler) => {
-        compiler.hooks.beforeCompile.tap('InjectWASM', () => {
-          // Write the WASM loader with the embedded binary
-          const wasmLoader = `
-// WASM Binary (base64 encoded)
-const wasmBase64 = "${wasmBase64}";
-
-// Convert base64 to array buffer
-function base64ToArrayBuffer(base64) {
-  const binaryString = atob(base64);
-  const bytes = new Uint8Array(binaryString.length);
-  for (let i = 0; i < binaryString.length; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
-  }
-  return bytes.buffer;
-}
-
-// Export the WASM binary
-export const embeddedWasmBinary = base64ToArrayBuffer(wasmBase64);
-`;
-          
-          fs.writeFileSync(
-            path.resolve(__dirname, 'src/wasm-loader.js'),
-            wasmLoader
-          );
-        });
-      }
-    }
+    })
   ],
   resolve: {
     extensions: ['.js', '.json', '.wasm'],
