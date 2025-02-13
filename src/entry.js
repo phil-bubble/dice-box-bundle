702524fc294eb272cc52f4eb280852f9c34a767a
phildaponte <p.daponte@outlook.com>
2025-02-12 21:51:09 -0500

    Fix DiceBox loading and initialization

--- src/entry.js
+++ src/entry.js
@@ -1,7 +1,7 @@
 import DiceBox from '@3d-dice/dice-box';
 
-// Export DiceBox as a global variable
-window.DiceBox = DiceBox;
+if (typeof window !== 'undefined') {
+    window.DiceBox = DiceBox;
+}
 
-// Also export as a module
 export default DiceBox;
